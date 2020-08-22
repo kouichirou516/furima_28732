022a5c7648424c32906921a3cd2aa62f16d9b123
kouichirou516 <69028982+kouichirou516@users.noreply.github.com>
2020-08-22 18:57:13 +0900

    Basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
 
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.basic_auth_user && password == Rails.application.credentials.basic_auth_password  # 環境変数を読み込む記述に変更
+    end
+  end
 end
