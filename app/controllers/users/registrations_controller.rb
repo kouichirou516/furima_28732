e864b918ff2d80a0a7c89a90c659537a61603cd3
kouichirou516 <69028982+kouichirou516@users.noreply.github.com>
2020-08-27 19:10:03 +0900

    新規登録ページの表示

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,4 +59,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def new
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'items#index'
+  # root to: 'items#index'
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations', 
+    sessions: "users/sessions"
+  } 
+  
+  devise_scope :user do
+    root "users/registrations#new"
+  end
+
+
 end
