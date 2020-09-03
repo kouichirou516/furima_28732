e9f26b5389676aed0714638cfeff907b65d1aa06
kouichirou516 <69028982+kouichirou516@users.noreply.github.com>
2020-09-03 14:34:03 +0900

    配送エリア、配送日数プルダウン実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,12 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
   belongs_to_active_hash :shipping_charge
   belongs_to_active_hash :area
+  belongs_to_active_hash :shipping_time
   #空の投稿を保存できないようにする
-  validates :image, :name, :category, :status, :shipping_charge, :area, presence: true
+  validates :image, :name, :category, :status, :shipping_charge, :area, :shipping_time, presence: true
 
   #ジャンルの選択が「--」の時は保存できないようにする
-  validates :category_id, :status_id, :shipping_charge, :area, numericality: { other_than: 1 }
+  validates :category_id, :status_id, :shipping_charge, :area, :shipping_time, numericality: { other_than: 1 }
 end
 
 
