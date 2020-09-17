9d04c680ea5deedeeb3f5f5120d957724f94a4cf
kouichirou516 <69028982+kouichirou516@users.noreply.github.com>
2020-09-17 19:25:46 +0900

    before_action :basic_auth修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(resource)
     root_path
@@ -13,8 +14,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
-
-  before_action :basic_auth
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
